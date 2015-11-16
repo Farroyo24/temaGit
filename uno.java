d10613d1b1f662e3be5bb8f7c6f6bb64a72c823b
Fran Arroyo <francisco.arroyoeliche@gmail.com>
2015-11-16 15:05:22 +0100

    Modificado en master 	modificado: uno.java

--- uno.java
+++ uno.java
@@ -1,7 +1,7 @@
 public class uno{
 		public static void main(String[] args){
 			System.out.println("Estamos en una nueva fase de desarrollo");
-			
+			System.out.println("Añado nueva linea en la rama master");
 			
 	
 	
